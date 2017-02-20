cba3465577e38dbed09fcdc2c84c00f55762e20e
PaulHerling <paul.herling_cottbus@gmx.de>
2017-02-20 20:42:42 +0100

    SQL

---
+++ src/main/java/Respository/Frage_resp.java
@@ -0,0 +1,18 @@
+package Respository;
+
+import java.sql.Connection;
+
+/**
+ * Created by Paul der Bademeister on 19.02.2017.
+ */
+public class Frage_resp {
+    /**
+    private final Connection conn;
+    public Frage_resp(Connection conn){
+        this.conn
+    }
+    */
+
+
+
+}
